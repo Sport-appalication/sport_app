db8f054fc45365d78e67fd4c5b37a71562249c5e
EmmanuelKeka <72016068+EmmanuelKeka@users.noreply.github.com>
2022-01-15 20:35:40 +0000

    Update mapActivity2.java

--- app/src/main/java/com/example/sport/mapActivity2.java
+++ app/src/main/java/com/example/sport/mapActivity2.java
@@ -2,13 +2,44 @@ package com.example.sport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class mapActivity2 extends AppCompatActivity {
-
+public class mapActivity2 extends AppCompatActivity implements View.OnClickListener {
+    private ImageView exercise,food,profil;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map2);
+        exercise = findViewById(R.id.exercise_link);
+        food = findViewById(R.id.food_link);
+        profil = findViewById(R.id.profil_link);
+        exercise.setOnClickListener(this);
+        food.setOnClickListener(this);
+        profil.setOnClickListener(this);
+        exercise.setImageResource(R.drawable.exercise_selected_icon);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.exercise_link:
+                intent = new Intent(this, AppPageActivity2.class);
+                startActivity(intent);
+                break;
+            case R.id.food_link:
+                intent = new Intent(this, NutritionActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.profil_link:
+                intent = new Intent(this, UserProfilActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
